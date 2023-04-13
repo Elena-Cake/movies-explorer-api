e27e5251125e25b77a3d0c7a119969e39d7fa7fa
Elena Sveshnikova <lenamarinkevich@mail.ru>
2023-04-13 12:03:20 +0300

    fix errors lint

--- app.js
+++ app.js
@@ -2,14 +2,13 @@ const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
 const cors = require('cors');
+const helmet = require('helmet');
 const bodyParser = require('body-parser');
 const routes = require('./routes');
-const helmet = require('helmet');
-
 const { PORT, DB_CONNECT_PATH } = require('./config');
-
 const errorHandler = require('./midldlewares/error-handler');
 const { requestLogger, errorLogger } = require('./midldlewares/logger');
+const { limiter } = require('./midldlewares/rateLimiter');
 
 mongoose
   .connect(DB_CONNECT_PATH)
@@ -29,6 +28,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use('*', cors());
 
 app.use(requestLogger);
+
+app.use(limiter);
 app.use(routes);
 app.use(errorLogger);
 
